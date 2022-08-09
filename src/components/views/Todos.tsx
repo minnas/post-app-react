409eee2c9dfa6fecca13239d893bdd61b201c938
Minna Salonen <minsalon@gmail.com>
2022-08-09 22:45:18 +0300

    added some dummy todo data

--- src/components/views/Todos.tsx
+++ src/components/views/Todos.tsx
@@ -1,6 +1,6 @@
-import { faBookAtlas, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faBookAtlas, faHome, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ApiType, search } from "../../api/api";
 import { Todo } from "../types/types";
@@ -12,7 +12,21 @@ const Home: React.FC<ViewProps> = ({
   }) => {
     const [todos, setTodos] = useState([] as Todo[]);
     const [count, setCount] = useState(0);
-
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      try {
+        setLoading(true);
+        search(ApiType.TODOS)
+          .then(items => {
+            setTodos(items as Todo[]);
+            setLoading(false);
+        });
+      } catch (error) {
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    });
     return (
       <div>
         <div className="content">
@@ -22,9 +36,22 @@ const Home: React.FC<ViewProps> = ({
               <FontAwesomeIcon icon={faBookAtlas} size="lg"/>
             </div>
             <div className="awesome-todo-list">
-              <h3>Current {count} todos in the list</h3>
+            <h3>Current {count} todos in the list</h3>
+            {
+              loading ? <div><FontAwesomeIcon icon={faSpinner} size="lg"/></div> : todos.map((todo:Todo) => (
+                <div className="todo-item-container" key={todos.indexOf(todo).toString()}>
+                  <div>{todos.indexOf(todo).toString()}</div>
+                  <div className="todo-title">
+                    { todo.title }
+                  </div>
+                  <div className="todo-title">
+                    Ready? { todo.completed }
+                  </div>
+                </div>
+              ))
+            }
             </div>
-            <div className="footer">
+            <div className="footer todos-footer">
               <NavLink className="footer-link home-link" to="/">
                 <FontAwesomeIcon icon={faHome} size="lg"/>
                   Back to Home
