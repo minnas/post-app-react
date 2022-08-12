2d11e61a0f9f5616a839acb3d7e64513759ecd80
Minna Salonen <minsalon@gmail.com>
2022-08-12 15:50:47 +0300

    added store

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import Home from "./components/views/Home"
 import Todos from "./components/views/Todos";
 import './custom.scss';
 import MyTodos from "./components/views/MyTodos";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 const App = (): JSX.Element => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="/my-todos" element={<MyTodos />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<Todos />} />
+          <Route path="/my-todos" element={<MyTodos />} />
+        </Routes>
+      </Provider>
     </Router>  
   );
 };
