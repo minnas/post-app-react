776af294fa18d102e3a104b7cb15ea633f591a39
Minna Salonen <minsalon@Minnas-MBP.home>
2022-10-14 13:33:29 +0300

    removed unneeded code

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 
-export const aliases = {
-  "@Api": path.resolve(__dirname, "src/api"),
-  "@Assets": path.resolve(__dirname, "src/assets"),
-  "@Styles": path.resolve(__dirname, "src/components/styles"),
-  "@tools": path.resolve(__dirname, "src/components/tools"),
-  "@Types": path.resolve(__dirname, "src/components/types"),
-  "@Views": path.resolve(__dirname, "src/components/views"),
-  "@Store": path.resolve(__dirname, "src/components/store"),
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
